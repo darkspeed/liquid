d9338a32037980867a138a39babea580da991c2d
Aaron Sutton <aaronjsutton@icloud.com>
2019-09-07 00:02:48 -0400

    Implement parser combinators and the field macro

--- lib/tls.ex
+++ lib/tls.ex
@@ -16,20 +16,43 @@ defmodule Liquid.TLS do
 
         @behaviour Request
         defoverridable Request
+
+        @type request_parser() :: {atom(), Keyword.t(), String.t()}
+
       end
     end
 
     @doc """
-    Constructs a request field function (RFF).
+    Constructs a request field function (RFF). Given the field name, `field` will generate
+    the appropriate `parse/2` function signature.
+
+    RFFs are "modular" functions responsible for parsing a particular field within a TLS request.
+    RFFs must implement the signature:
+
+    ```parse(atom(), request_parser()) :: request_parser()```
+
+    Where `request_parser()` is a 3-tuple containing an :ok/:error atom, the current parsed request,
+    and the remaining string to be parsed.
+
+    The implementor's `parse/1` method then dispatches, in order, the `parse/2` for every request name
+    defined in the implementor's `fields/0` function to build the request.
     """
     defmacro field(name, do: body) do
       quote do
         body = unquote(body)
-        defp(field(unquote(:"#{name}")), do: unquote(body))
+        defp parse(unquote(:"#{name}"), {:ok, _value, stream} = parse), do: unquote(body)
       end
     end
 
-    def parse(type) do
+    defp error(field, description), do: {:error, nil, "Error parsing field '#{field}': #{description}"}
+
+    # TODO: Pattern matching order?
+    defp parse(_name, {:error, _, _ } = error), do: error
+
+    # TODO: Decide on how to name these so defmacro, defp, and def don't collide.
+    def pub_parse(type, string) do
+      Enum.reduce(type.fields(), {:ok, nil, string}, fn field, parser ->
+        type.parse(field, parser) end )
     end
   end
 
