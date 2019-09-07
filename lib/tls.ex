e7fc5451df6c4ab9c763e40cb6ddf16dd0118fa9
Aaron Sutton <aaronjsutton@icloud.com>
2019-09-06 22:41:25 -0400

    Field macro, Request module structure, swap file ignore

--- lib/tls.ex
+++ lib/tls.ex
@@ -1,28 +1,44 @@
-defmodule Liquid.TLS.Handshake do
+defmodule Liquid.TLS do
   @moduledoc """
   Liquid's implementation of the TLS 1.2 Handshake Protocol.
   See [the specification](https://tools.ietf.org/html/rfc5246#section-1.2)
   for more information.
   """
 
-  @doc """
-  Maps the given handshake type, as an atom or a string, to it's corresponding numerical value 
-  by [§7.4. Handshake Protocol](https://tools.ietf.org/html/rfc5246#section-7.4).
-  """
-  def type(handshake) when is_atom(handshake) do
-    case handshake do
-      :hello_request -> 0x0
-      :client_hello -> 0x1
-      :server_hello -> 0x2
-      :certificate -> 0xB
-      :server_key_exchange -> 0xC
-      :certificate_request -> 0xD
-      :server_hello_done -> 0xE
-      :certificate_verify -> 0xF
-      :client_key_exchange -> 0x10
-      :finished -> 0x14
+  defmodule Request do
+    @callback type() :: {atom(), byte()}
+    @callback fields() :: [atom()]
+
+    @doc false
+    defmacro __using__(_opts) do
+      quote do
+        import Request
+
+        @behaviour Request
+        defoverridable Request
+      end
+    end
+
+    @doc """
+    Constructs a request field function (RFF).
+    """
+    defmacro field(name, do: body) do
+      quote do
+        body = unquote(body)
+        defp(field(unquote(:"#{name}")), do: unquote(body))
+      end
+    end
+
+    def parse(type) do
     end
   end
 
-  def type(handshake), do: type(List.to_atom(handshake))
+  defmodule TestReq do
+    use Liquid.TLS.Request
+
+    field :test do
+      1 + 1
+    end
+
+  end
 end
