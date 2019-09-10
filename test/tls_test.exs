b81961dcdbed387e403592f6b8d916b63b1b07d3
Aaron Sutton <aaronjsutton@icloud.com>
2019-09-10 14:24:57 -0400

    Define record layer parsing function and tests
    commit 02c2c40ef4a95d247bb12b0d907a49993501229c
    Merge: 6db8d8e fda0fe9
    Author: Aaron Sutton <aaronjsutton@icloud.com>
    Date:   Tue Sep 10 14:24:33 2019 -0400
    
        Merge branch 'develop' into local
    
    commit 6db8d8e38a58d71ab27edf3f07bf5605e75c40e9
    Author: Aaron Sutton <aaronjsutton@icloud.com>
    Date:   Tue Sep 10 14:20:11 2019 -0400
    
        Complete parse/2 for :record_layer
    
    commit eca7a0c836d3f705cb5267c5a527e74bd72ff13d
    Author: Aaron Sutton <aaronjsutton@icloud.com>
    Date:   Tue Sep 10 11:35:19 2019 -0400
    
        Change argument name to input in field macro
    
    commit 93193b18c1ddc24d77c374b9fbe125b63db74015
    Author: Aaron Sutton <aaronjsutton@icloud.com>
    Date:   Tue Sep 10 11:34:41 2019 -0400
    
        @type define input()
    
    commit c1c015f2ed5ed1eff2c9277b6ec8fae3a4f6c0f8
    Author: Aaron Sutton <aaronjsutton@icloud.com>
    Date:   Tue Sep 10 07:02:49 2019 -0400
    
        Add spec and docs
    
    commit e83d6090e35058e2902e30308c53bd28b5e79d6f
    Author: Aaron Sutton <aaronjsutton@icloud.com>
    Date:   Tue Sep 10 00:40:40 2019 -0400
    
        Rewrite documentation

--- test/tls_test.exs
+++ test/tls_test.exs
@@ -4,15 +4,24 @@ defmodule TLSTest do
   import Liquid.TLS.Request
 
   describe "request parsing" do
+<<<<<<< HEAD
     defp test_parse(req), do: parse(:record_layer, {:ok, [], req})
 
     test "parses valid record layer" do
       record_header = <<0x16, 0x03, 0x01, 0x00, 0xA5>>
+=======
+
+    defp test_parse(req), do: parse(:record_layer, {:ok, [], req})
+
+    test "parses valid record layer" do
+      record_header = <<0x16, 0x03, 0x01, 0x00, 0xa5>>
+>>>>>>> Define record layer parsing function and tests
       result = test_parse(record_header)
       assert result == {:ok, [record_layer: [version: {3, 1}, length: 165]], <<>>}
     end
 
     test "fails on incorrect TLS version" do
+<<<<<<< HEAD
       bad_version = <<0x16, 0xFF, 0xFF, 0x00, 0xA5>>
       result = test_parse(bad_version)
       refute match?({:ok, _, _}, result)
@@ -28,6 +37,17 @@ defmodule TLSTest do
       request = <<0x16, 0x03, 0x01, 0x00, 0xA5, 0xFF, 0xFF>>
       {_, _, remaining} = test_parse(request)
       assert remaining == <<0xFF, 0xFF>>
+=======
+      bad_version = <<0x16, 0xFF, 0xFF, 0x00, 0xa5>>
+      result = test_parse bad_version
+      refute match?({:ok, _, _}, result)
+    end
+
+    test "fails on malformed request" do
+      bad_request = <<0, 0, 4, 4, 1>>
+      result = test_parse bad_request
+      refute match?({:ok, _, _}, result)
+>>>>>>> Define record layer parsing function and tests
     end
   end
 end
